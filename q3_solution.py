4d3940aab36a4422a739fa3c1c9b170d4a9f88ec
Samuel Aguilar-Lussier <sam.aglussier@gmail.com>
2022-04-02 14:28:52 -0400

    cosine similarity correction

--- q3_solution.py
+++ q3_solution.py
@@ -144,6 +144,6 @@ class CosineSimilarity(Module):
         """
         COMPLETE ME. DONT MODIFY THE PARAMETERS OF THE FUNCTION. Otherwise, tests might fail.
         """
-        cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+        cos = nn.CosineSimilarity(dim=self.dim, eps=self.eps)
 
         return cos(x1, x2)
